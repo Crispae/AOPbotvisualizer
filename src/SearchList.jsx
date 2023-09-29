b349d9e8a0194a6cb5a7e4dca37ecc405023686d
Crispae <sritwik396@gmail.com>
2023-09-29 12:02:32 +0200

    added steps to implement fuse.js

--- src/SearchList.jsx
+++ src/SearchList.jsx
@@ -1,1 +1,19 @@
 // Component to display the dropdown of recomendation of entites mentioned here
+import React from 'react'
+
+function SearchList() {
+
+/*
+0. When network load, also at that time we need to load all the input in search box
+1. Capture the user's input in search box
+2. Feed captured input in fuse.js to list the top 5 enites
+3. Feed those entites in a a seprate component, whic will take this and return a component having name,raw_name,id
+4. user can click on it and depneding on the display name they have selected, node selection will be done.
+*/
+
+  return (
+    <div>SearchList</div>
+  )
+}
+
+export default SearchList
